66ce10f85deafb7b84221246e7f895d6064a22ec
Stefan Marr <git@stefan-marr.de>
2014-09-18 18:12:20 +0200

    Remove type specialization of frame slots.
    Signed-off-by: Stefan Marr <git@stefan-marr.de>
    
    Conflicts:
    	src/som/interpreter/nodes/LocalVariableNode.java
    	src/som/interpreter/nodes/NonLocalVariableNode.java

--- src/som/interpreter/nodes/NonLocalVariableNode.java
+++ src/som/interpreter/nodes/NonLocalVariableNode.java
@@ -12,7 +12,6 @@ import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.FrameSlotKind;
-import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.source.SourceSection;
 
@@ -43,31 +42,11 @@ public abstract class NonLocalVariableNode extends ContextualNode {
       return Nil.nilObject;
     }
 
-    @Specialization(guards = "isInitialized", rewriteOn = {FrameSlotTypeException.class})
-    public final boolean doBoolean(final VirtualFrame frame) throws FrameSlotTypeException {
-      return determineContext(frame).getBoolean(slot);
-    }
 
-    @Specialization(guards = "isInitialized", rewriteOn = {FrameSlotTypeException.class})
-    public final long doLong(final VirtualFrame frame) throws FrameSlotTypeException {
-      return determineContext(frame).getLong(slot);
+    @Specialization(guards = "isInitialized")
+    public final Object doObject(final VirtualFrame frame) {
+      return determineContext(frame).getValue(slot);
     }
-
-    @Specialization(guards = "isInitialized", rewriteOn = {FrameSlotTypeException.class})
-    public final double doDouble(final VirtualFrame frame) throws FrameSlotTypeException {
-      return determineContext(frame).getDouble(slot);
-    }
-
-    @Specialization(guards = "isInitialized", rewriteOn = {FrameSlotTypeException.class})
-    public final Object doObject(final VirtualFrame frame) throws FrameSlotTypeException {
-      return determineContext(frame).getObject(slot);
-    }
-
-//    @Generic
-//    public final Object doGeneric(final VirtualFrame frame) {
-//      assert isInitialized();
-//      return FrameUtil.getObjectSafe(determineContext(frame), slot);
-//    }
 
     protected final boolean isInitialized() {
       return slot.getKind() != FrameSlotKind.Illegal;
@@ -82,8 +61,8 @@ public abstract class NonLocalVariableNode extends ContextualNode {
       FrameSlot varSlot = inliner.getLocalFrameSlot(this.slot.getIdentifier());
       replace(NonLocalVariableReadNodeFactory.create(contextLevel, varSlot,
          getSourceSection()));
-    }
   }
+    }
 
   public abstract static class NonLocalSuperReadNode
                        extends NonLocalVariableReadNode implements ISuperReadNode {
@@ -132,65 +111,11 @@ public abstract class NonLocalVariableNode extends ContextualNode {
 
     protected abstract ExpressionNode getExp();
 
-    @Specialization(guards = "isBoolKind")
-    public final boolean writeBoolean(final VirtualFrame frame, final boolean expValue) {
-      determineContext(frame).setBoolean(slot, expValue);
-      return expValue;
-    }
-
-    @Specialization(guards = "isLongKind")
-    public final long writeLong(final VirtualFrame frame, final long expValue) {
-      determineContext(frame).setLong(slot, expValue);
-      return expValue;
-    }
-
-    @Specialization(guards = "isDoubleKind")
-    public final double writeDouble(final VirtualFrame frame, final double expValue) {
-      determineContext(frame).setDouble(slot, expValue);
-      return expValue;
-    }
-
-    @Specialization(contains = {"writeBoolean", "writeLong", "writeDouble"})
-    public final Object writeGeneric(final VirtualFrame frame, final Object expValue) {
+    @Specialization
+    public final Object write(final VirtualFrame frame, final Object expValue) {
       ensureObjectKind();
       determineContext(frame).setObject(slot, expValue);
       return expValue;
-    }
-
-    protected final boolean isBoolKind() {
-      if (slot.getKind() == FrameSlotKind.Boolean) {
-        return true;
-      }
-      if (slot.getKind() == FrameSlotKind.Illegal) {
-        transferToInterpreter("LocalVar.writeBoolToUninit");
-        slot.setKind(FrameSlotKind.Boolean);
-        return true;
-      }
-      return false;
-    }
-
-    protected final boolean isLongKind() {
-      if (slot.getKind() == FrameSlotKind.Long) {
-        return true;
-      }
-      if (slot.getKind() == FrameSlotKind.Illegal) {
-        transferToInterpreter("LocalVar.writeIntToUninit");
-        slot.setKind(FrameSlotKind.Long);
-        return true;
-      }
-      return false;
-    }
-
-    protected final boolean isDoubleKind() {
-      if (slot.getKind() == FrameSlotKind.Double) {
-        return true;
-      }
-      if (slot.getKind() == FrameSlotKind.Illegal) {
-        transferToInterpreter("LocalVar.writeDoubleToUninit");
-        slot.setKind(FrameSlotKind.Double);
-        return true;
-      }
-      return false;
     }
 
     protected final void ensureObjectKind() {
@@ -205,6 +130,6 @@ public abstract class NonLocalVariableNode extends ContextualNode {
       FrameSlot varSlot   = inliner.getLocalFrameSlot(this.slot.getIdentifier());
       replace(NonLocalVariableWriteNodeFactory.create(contextLevel, varSlot,
          getSourceSection(), getExp()));
-    }
   }
+}
 }
