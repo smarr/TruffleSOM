5c33cbe3950bc1965b42d3ee04c2f353e98666bc
Stefan Marr <git@stefan-marr.de>
2014-09-18 18:12:20 +0200

    Remove type specialization of frame slots.
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/LocalVariableNode.java
+++ src/som/interpreter/nodes/LocalVariableNode.java
@@ -15,7 +15,6 @@ import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.FrameSlotKind;
-import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.source.SourceSection;
 
@@ -53,32 +52,10 @@ public abstract class LocalVariableNode extends ExpressionNode {
       return Nil.nilObject;
     }
 
-    @Specialization(guards = "isInitialized", rewriteOn = {FrameSlotTypeException.class})
-    public final boolean doBoolean(final VirtualFrame frame) throws FrameSlotTypeException {
-      return frame.getBoolean(slot);
+    @Specialization(guards = "isInitialized")
+    public final Object doObject(final VirtualFrame frame) {
+      return frame.getValue(slot);
     }
-
-
-    @Specialization(guards = "isInitialized", rewriteOn = {FrameSlotTypeException.class})
-    public final long doLong(final VirtualFrame frame) throws FrameSlotTypeException {
-      return frame.getLong(slot);
-    }
-
-    @Specialization(guards = "isInitialized", rewriteOn = {FrameSlotTypeException.class})
-    public final double doDouble(final VirtualFrame frame) throws FrameSlotTypeException {
-      return frame.getDouble(slot);
-    }
-
-    @Specialization(guards = "isInitialized", rewriteOn = {FrameSlotTypeException.class})
-    public final Object doObject(final VirtualFrame frame) throws FrameSlotTypeException {
-      return frame.getObject(slot);
-    }
-
-//    @Generic
-//    public final Object doGeneric(final VirtualFrame frame) {
-//      assert isInitialized();
-//      return FrameUtil.getObjectSafe(frame, slot);
-//    }
 
     protected final boolean isInitialized() {
       return slot.getKind() != FrameSlotKind.Illegal;
@@ -146,65 +123,11 @@ public abstract class LocalVariableNode extends ExpressionNode {
 
     public abstract ExpressionNode getExp();
 
-    @Specialization(guards = "isBoolKind")
-    public final boolean writeBoolean(final VirtualFrame frame, final boolean expValue) {
-      frame.setBoolean(slot, expValue);
-      return expValue;
-    }
-
-    @Specialization(guards = "isLongKind")
-    public final long writeLong(final VirtualFrame frame, final long expValue) {
-      frame.setLong(slot, expValue);
-      return expValue;
-    }
-
-    @Specialization(guards = "isDoubleKind")
-    public final double writeDouble(final VirtualFrame frame, final double expValue) {
-      frame.setDouble(slot, expValue);
-      return expValue;
-    }
-
-    @Specialization(contains = {"writeBoolean", "writeLong", "writeDouble"})
+    @Specialization
     public final Object writeGeneric(final VirtualFrame frame, final Object expValue) {
       ensureObjectKind();
       frame.setObject(slot, expValue);
       return expValue;
-    }
-
-    protected final boolean isBoolKind() {
-      if (slot.getKind() == FrameSlotKind.Boolean) {
-        return true;
-      }
-      if (slot.getKind() == FrameSlotKind.Illegal) {
-        transferToInterpreter("LocalVar.writeBoolToUninit");
-        slot.setKind(FrameSlotKind.Boolean);
-        return true;
-      }
-      return false;
-    }
-
-    protected final boolean isLongKind() {
-      if (slot.getKind() == FrameSlotKind.Long) {
-        return true;
-      }
-      if (slot.getKind() == FrameSlotKind.Illegal) {
-        transferToInterpreter("LocalVar.writeIntToUninit");
-        slot.setKind(FrameSlotKind.Long);
-        return true;
-      }
-      return false;
-    }
-
-    protected final boolean isDoubleKind() {
-      if (slot.getKind() == FrameSlotKind.Double) {
-        return true;
-      }
-      if (slot.getKind() == FrameSlotKind.Illegal) {
-        transferToInterpreter("LocalVar.writeDoubleToUninit");
-        slot.setKind(FrameSlotKind.Double);
-        return true;
-      }
-      return false;
     }
 
     protected final void ensureObjectKind() {
