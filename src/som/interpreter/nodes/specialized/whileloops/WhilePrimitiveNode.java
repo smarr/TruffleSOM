d5c98a5b5a8b1d6667017fcb7729fbf5ab7f6a8c
Stefan Marr <git@stefan-marr.de>
2014-10-20 14:06:28 +0200

    Remove all the while-node infrastructure
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/specialized/whileloops/WhilePrimitiveNode.java
+++ src/som/interpreter/nodes/specialized/whileloops/WhilePrimitiveNode.java
@@ -1,7 +1,8 @@
 package som.interpreter.nodes.specialized.whileloops;
 
 import som.interpreter.nodes.nary.BinaryExpressionNode;
-import som.interpreter.nodes.specialized.whileloops.WhileCache.AbstractWhileDispatch;
+import som.vm.constants.Globals;
+import som.vm.constants.Nil;
 import som.vmobjects.SBlock;
 import som.vmobjects.SObject;
 
@@ -11,12 +12,10 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 
 public abstract class WhilePrimitiveNode extends BinaryExpressionNode {
   final boolean predicateBool;
-  @Child protected AbstractWhileDispatch whileNode;
 
   protected WhilePrimitiveNode(final boolean predicateBool) {
     super(null);
     this.predicateBool = predicateBool;
-    this.whileNode = WhileCache.create(predicateBool);
   }
 
   protected WhilePrimitiveNode(final WhilePrimitiveNode node) {
@@ -26,10 +25,30 @@ public abstract class WhilePrimitiveNode extends BinaryExpressionNode {
   @Specialization
   protected SObject doWhileConditionally(final VirtualFrame frame,
       final SBlock loopCondition, final SBlock loopBody) {
-    return whileNode.executeDispatch(frame, loopCondition, loopBody);
+    Object conditionResult = loopCondition.getMethod().invoke(loopCondition);
+    boolean loopConditionResult = obj2bool(conditionResult);
+
+    // TODO: this is a simplification, we don't cover the case receiver isn't a boolean
+    while (loopConditionResult == predicateBool) {
+      loopBody.getMethod().invoke(loopBody);
+      conditionResult = loopCondition.getMethod().invoke(loopCondition);
+      loopConditionResult = obj2bool(conditionResult);
+    }
+    return Nil.nilObject;
   }
 
   public abstract static class WhileTruePrimitiveNode extends WhilePrimitiveNode {
     public WhileTruePrimitiveNode() { super(true); }
+  }
+
+  private boolean obj2bool(final Object o) {
+    if (o instanceof Boolean) {
+      return (boolean) o;
+    } else if (o == Globals.trueObject) {
+      return true;
+    } else {
+      assert o == Globals.falseObject;
+      return false;
+    }
   }
 }
