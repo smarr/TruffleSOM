f52f56120b1cb7f74627ab7df1bb35e34b373be0
Stefan Marr <git@stefan-marr.de>
2015-10-06 22:29:35 +0200

    Replace old scripts with Python version
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# -G:TruffleCompilationThreshold=3 -Xbootclasspath/a:build/classes \
-java -server -cp build/classes:libs/truffle/build/truffle-api.jar \
-        som.vm.Universe \
-		"$@"
