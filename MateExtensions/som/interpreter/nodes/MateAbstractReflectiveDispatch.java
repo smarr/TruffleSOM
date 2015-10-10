b6525188dfbcdd7c71fd0c2f074083762f841f8f
Stefan Marr <git@stefan-marr.de>
2015-10-10 08:51:32 +0200

    Eclipse format cleanup
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- MateExtensions/som/interpreter/nodes/MateAbstractReflectiveDispatch.java
+++ MateExtensions/som/interpreter/nodes/MateAbstractReflectiveDispatch.java
@@ -3,7 +3,6 @@ package som.interpreter.nodes;
 import som.interpreter.SArguments;
 import som.interpreter.nodes.MateAbstractReflectiveDispatchFactory.MateDispatchActivationNodeGen;
 import som.interpreter.nodes.MessageSendNode.AbstractMessageSendNode;
-import som.interpreter.objectstorage.FieldAccessorNode;
 import som.interpreter.objectstorage.FieldAccessorNode.AbstractReadFieldNode;
 import som.interpreter.objectstorage.FieldAccessorNode.AbstractWriteFieldNode;
 import som.vm.MateUniverse;
@@ -36,38 +35,38 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
   public MateAbstractReflectiveDispatch(final SourceSection source){
     super(source);
   }
-  
+
   /*public Object[] evaluateArguments(final VirtualFrame frame) {
     Object[] receiver = new Object[1];
     receiver[0] = SArguments.rcvr(frame);
     return receiver;
   }*/
-       
-  public DirectCallNode createDispatch(SMateEnvironment metaobject){
+
+  public DirectCallNode createDispatch(final SMateEnvironment metaobject){
     SInvokable target = metaobject.methodImplementing(this.getReflectiveOperation());
     if (target != null){
        return MateUniverse.current().getTruffleRuntime().createDirectCallNode(target.getCallTarget());
     }
     return null;
   }
-  
+
   protected abstract ReflectiveOp getReflectiveOperation();
-  
+
   public abstract static class MateDispatchFieldAccessor extends MateAbstractReflectiveDispatch {
     public MateDispatchFieldAccessor(final SourceSection source) {
       super(source);
     }
-    
+
     public abstract Object executeDispatch(final VirtualFrame frame, Object environment, Object[] arguments);
-    public Object doWrappedNode(Object[] arguments){return null;}
-     
+    public Object doWrappedNode(final Object[] arguments){return null;}
+
     @Specialization(guards = "cachedEnvironment==environment")
-    public Object doMateNode(final VirtualFrame frame,  
-        SMateEnvironment environment,
-        Object[] arguments,
-        @Cached("environment") SMateEnvironment cachedEnvironment,
-        @Cached("createDispatch(environment)") DirectCallNode reflectiveMethod) 
-    { 
+    public Object doMateNode(final VirtualFrame frame,
+        final SMateEnvironment environment,
+        final Object[] arguments,
+        @Cached("environment") final SMateEnvironment cachedEnvironment,
+        @Cached("createDispatch(environment)") final DirectCallNode reflectiveMethod)
+    {
         if (reflectiveMethod != null) {
           MateUniverse.current().enterMetaExecutionLevel();
           Object value = reflectiveMethod.call(frame, arguments);
@@ -76,15 +75,16 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
         }
         return doSomNode(frame, environment, arguments);
     }
-    
+
     @Specialization(guards= "environment == null")
-    public Object doSomNode(final VirtualFrame frame, Object environment, Object[] arguments){ 
+    public Object doSomNode(final VirtualFrame frame, final Object environment, final Object[] arguments){
         return this.doWrappedNode(arguments);
     }
-    
+
+    @Override
     protected ReflectiveOp getReflectiveOperation(){return null;}
   }
-  
+
   public abstract static class MateDispatchFieldReadLayout extends MateDispatchFieldAccessor {
     @Child protected AbstractReadFieldNode wrappedNode;
 
@@ -92,17 +92,18 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
       super(node.getSourceSection());
       wrappedNode = node;
     }
-    
+
+    @Override
     protected ReflectiveOp getReflectiveOperation(){
       return this.wrappedNode.reflectiveOperation();
     }
-    
+
     @Override
-    public Object doWrappedNode(Object[] arguments){
+    public Object doWrappedNode(final Object[] arguments){
       return this.wrappedNode.read((SObject)arguments[0]);
     }
   }
-  
+
   public abstract static class MateDispatchFieldWriteLayout extends MateDispatchFieldAccessor {
     @Child protected AbstractWriteFieldNode wrappedNode;
 
@@ -110,17 +111,18 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
       super(node.getSourceSection());
       wrappedNode = node;
     }
-    
+
     @Override
-    public Object doWrappedNode(Object[] arguments){
+    public Object doWrappedNode(final Object[] arguments){
       return this.wrappedNode.write((SObject)arguments[0], arguments[2]);
     }
-    
+
+    @Override
     protected ReflectiveOp getReflectiveOperation(){
       return this.wrappedNode.reflectiveOperation();
-    }  
+    }
   }
-  
+
   public abstract static class MateDispatchFieldAccess extends MateAbstractReflectiveDispatch {
     @Child protected FieldNode wrappedNode;
 
@@ -128,39 +130,40 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
       super(node.getSourceSection());
       wrappedNode = node;
     }
-    
+
     public abstract Object executeDispatch(final VirtualFrame frame, Object environment, Object receiver);
-    
+
     @Specialization(guards= "environment == null")
-    public Object doSomNode(final VirtualFrame frame, Object environment, Object receiver){
+    public Object doSomNode(final VirtualFrame frame, final Object environment, final Object receiver){
       return this.doWrappedNode(frame);
     }
-    
+
     @Specialization(guards = {"cachedEnvironment==environment"})
-    public Object doMetaLevel(final VirtualFrame frame,  
-        SMateEnvironment environment,
-        SObject receiver,
-        @Cached("environment") SMateEnvironment cachedEnvironment,
-        @Cached("createDispatch(environment)") DirectCallNode reflectiveMethod) 
-    { 
+    public Object doMetaLevel(final VirtualFrame frame,
+        final SMateEnvironment environment,
+        final SObject receiver,
+        @Cached("environment") final SMateEnvironment cachedEnvironment,
+        @Cached("createDispatch(environment)") final DirectCallNode reflectiveMethod)
+    {
         if (reflectiveMethod != null){
           MateUniverse.current().enterMetaExecutionLevel();
           Object value = reflectiveMethod.call(frame, this.wrappedNode.argumentsForReceiver(frame, receiver));
           MateUniverse.current().leaveMetaExecutionLevel();
           return value;
         }
-        return this.doWrappedNode(frame); 
+        return this.doWrappedNode(frame);
     }
-    
+
     public Object doWrappedNode(final VirtualFrame frame){
       return this.wrappedNode.executeGeneric(frame);
     }
-    
+
+    @Override
     protected ReflectiveOp getReflectiveOperation(){
       return this.wrappedNode.reflectiveOperation();
-    }  
-  }  
-  
+    }
+  }
+
   public abstract static class MateDispatchMessageLookup extends MateAbstractReflectiveDispatch {
     @Child protected ExpressionWithReceiverNode wrappedNode;
     @Child MateDispatchActivation activationDispatch;
@@ -170,21 +173,21 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
       this.wrappedNode = node;
       activationDispatch = MateDispatchActivationNodeGen.create(node);
     }
-    
+
     public abstract Object executeDispatch(final VirtualFrame frame, Object environment, Object receiver);
-    
+
     @Specialization(guards= "environment == null")
-    public Object doSomNode(final VirtualFrame frame, Object environment, Object receiver){
+    public Object doSomNode(final VirtualFrame frame, final Object environment, final Object receiver){
       return this.doWrappedNode(frame);
     }
-    
+
     @Specialization(guards = {"(cachedEnvironment==environment)"})
-    public Object doMetaLevel(final VirtualFrame frame,  
-        SMateEnvironment environment,
-        SObject receiver,
-        @Cached("environment") SMateEnvironment cachedEnvironment,
-        @Cached("createDispatch(environment)") DirectCallNode reflectiveMethod) 
-    { 
+    public Object doMetaLevel(final VirtualFrame frame,
+        final SMateEnvironment environment,
+        final SObject receiver,
+        @Cached("environment") final SMateEnvironment cachedEnvironment,
+        @Cached("createDispatch(environment)") final DirectCallNode reflectiveMethod)
+    {
         if (reflectiveMethod != null){
           //The MOP receives the class where the lookup must start (find: aSelector since: aClass)
           MateUniverse.current().enterMetaExecutionLevel();
@@ -195,24 +198,25 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
         }
         return this.doWrappedNode(frame);
     }
-    
+
+    @Override
     protected ReflectiveOp getReflectiveOperation(){
       return this.wrappedNode.reflectiveOperation();
     }
-    
+
     public AbstractMessageSendNode getSOMWrappedNode(){
       return (AbstractMessageSendNode)this.wrappedNode;
     }
-    
+
     protected SSymbol getSelector(){
       return ((AbstractMessageSendNode)this.wrappedNode).getSelector();
     }
-    
+
     public Object doWrappedNode (final VirtualFrame frame){
       return this.wrappedNode.executeGeneric(frame);
     }
   }
-  
+
   public abstract static class MateDispatchActivation extends MateAbstractReflectiveDispatch {
     protected final ExpressionWithReceiverNode wrappedNode;
 
@@ -220,27 +224,28 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
       super(node.getSourceSection());
       wrappedNode = node;
     }
-    
+
     public abstract Object executeDispatch(final VirtualFrame frame, SMateEnvironment environment, Object receiver, SMethod methodToCall);
-    
+
+    @Override
     protected ReflectiveOp getReflectiveOperation(){
       return ReflectiveOp.Activation;
-    }  
+    }
 
     @Specialization(guards= "environment == null")
-    public Object doSomNode(final VirtualFrame frame, SMateEnvironment environment, SObject receiver, SMethod methodToCall){
+    public Object doSomNode(final VirtualFrame frame, final SMateEnvironment environment, final SObject receiver, final SMethod methodToCall){
       return methodToCall.getCallTarget().call(((PreevaluatedExpression) wrappedNode).evaluateArguments(frame));
     }
-    
+
     /*Todo: Optimize: Isn't the operation always fixes*/
     @Specialization(guards = "(cachedEnvironment==environment)")
-    public Object doMetaLevel(final VirtualFrame frame,  
-        SMateEnvironment environment,
-        SObject receiver,
-        SMethod methodToCall,
-        @Cached("environment") SMateEnvironment cachedEnvironment,
-        @Cached("createDispatch(environment)") DirectCallNode reflectiveMethod) 
-    { 
+    public Object doMetaLevel(final VirtualFrame frame,
+        final SMateEnvironment environment,
+        final SObject receiver,
+        final SMethod methodToCall,
+        @Cached("environment") final SMateEnvironment cachedEnvironment,
+        @Cached("createDispatch(environment)") final DirectCallNode reflectiveMethod)
+    {
       Object[] arguments = ((PreevaluatedExpression) wrappedNode).evaluateArguments(frame);
       if (reflectiveMethod != null){
         //The MOP receives the standard ST message Send stack (rcvr, selector, arguments) and return its own
@@ -260,41 +265,45 @@ public abstract class MateAbstractReflectiveDispatch extends Node {
           } finally {
             runtime.popFrame();
           }
-        }  
+        }
         return methodToCall.getCallTarget().call(realArguments);
       } else {
         return methodToCall.getCallTarget().call(arguments);
       }
     }
-    
-    private Object[] gatherArrayFromSArray(SArray array){
+
+    private Object[] gatherArrayFromSArray(final SArray array){
       if (array.getType() == ArrayType.PARTIAL_EMPTY){
         return array.getPartiallyEmptyStorage(ValueProfile.createClassProfile()).getStorage();
       } else {
         return array.getObjectStorage(ValueProfile.createClassProfile());
       }
     }
-    
-    private FrameInstance frameInstanceFor(VirtualFrame frame, SMethod method){
+
+    private FrameInstance frameInstanceFor(final VirtualFrame frame, final SMethod method){
       return new FrameInstance() {
-        public Frame getFrame(FrameAccess access, boolean slowPath) {
+        @Override
+        public Frame getFrame(final FrameAccess access, final boolean slowPath) {
             return frame;
         }
 
+        @Override
         public boolean isVirtualFrame() {
             return false;
         }
 
+        @Override
         public Node getCallNode() {
             return method.getCallTarget().getRootNode();
         }
 
+        @Override
         public CallTarget getCallTarget() {
             return method.getCallTarget();
         }
       };
     }
-    private VirtualFrame newFrameWithRedefinedSemanticsFor(Object[] arguments, SMethod methodToCall, SMateEnvironment environment, DefaultTruffleRuntime runtime){
+    private VirtualFrame newFrameWithRedefinedSemanticsFor(final Object[] arguments, final SMethod methodToCall, final SMateEnvironment environment, final DefaultTruffleRuntime runtime){
       VirtualFrame customizedFrame = runtime.createVirtualFrame(arguments, methodToCall.getCallTarget().getRootNode().getFrameDescriptor());
       FrameSlot slot = customizedFrame.getFrameDescriptor().addFrameSlot("semantics", FrameSlotKind.Object);
       customizedFrame.setObject(slot, environment);
