4002e77481d5d712dfb863e0dd7874de09cf88eb
Guido Chari <charig@gmail.com>
2015-10-16 17:00:06 -0300

    Resolving conflict

---
+++ MateExtensions/som/vm/MateUniverse.java
@@ -0,0 +1,101 @@
+package som.vm;
+
+import static som.vm.constants.MateClasses.environmentMO;
+import static som.vm.constants.MateClasses.operationalSemanticsMO;
+import static som.vm.constants.MateClasses.messageMO;
+import static som.vm.constants.MateClasses.ShapeClass;
+import static som.vm.constants.Classes.objectClass;
+import som.interpreter.Invokable;
+import som.interpreter.MateifyVisitor;
+import som.vmobjects.SClass;
+import som.vmobjects.SInvokable;
+import som.vmobjects.SMateEnvironment;
+import som.vmobjects.SReflectiveObject;
+import som.vmobjects.SSymbol;
+
+public class MateUniverse extends Universe {
+  
+  protected boolean executingMeta;
+  
+  protected void initializeObjectSystem() {
+    if (alreadyInitialized) {
+      return;
+    } else {
+      super.initializeObjectSystem();
+      
+      // Initialize the Mate metamodel.
+      initializeSystemClass(environmentMO, objectClass, "EnvironmentMO");
+      initializeSystemClass(operationalSemanticsMO, objectClass, "OperationalSemanticsMO");
+      initializeSystemClass(messageMO, objectClass, "MessageMO");
+      initializeSystemClass(ShapeClass, objectClass, "Shape");
+      
+      // Load methods and fields into the Mate MOP.
+      loadSystemClass(environmentMO);
+      loadSystemClass(operationalSemanticsMO);
+      loadSystemClass(messageMO);
+      loadSystemClass(ShapeClass);
+      
+      this.executingMeta = false;
+    }
+  }
+  
+  public static SReflectiveObject newInstance(final SClass instanceClass) {
+    return SReflectiveObject.create(instanceClass);
+  }
+  
+  public static SMateEnvironment newEnvironment(final SClass instanceClass) {
+    return SMateEnvironment.create(instanceClass);
+  }
+  
+  public SClass loadClass(final SSymbol name) {
+    if ((SClass) getGlobal(name) != null){
+      return super.loadClass(name);
+    } else {
+      SClass result = super.loadClass(name);
+      mateify(name);
+      return result;
+    }
+  }
+  
+  public void mateify(final SSymbol name) {
+    SClass clazz = (SClass) getGlobal(name);
+    //MateUniverse.current().println(name.getString());
+    int countOfInvokables = clazz.getNumberOfInstanceInvokables();
+    MateifyVisitor visitor = new MateifyVisitor();
+    for (int i = 0; i < countOfInvokables; i++){
+      SInvokable method = clazz.getInstanceInvokable(i);
+      Invokable node = method.getInvokable();
+      node.accept(visitor);
+    }
+  }
+  
+  public static void main(final String[] arguments) {
+    MateUniverse u = current();
+
+    try {
+      u.interpret(arguments);
+      u.exit(0);
+    } catch (IllegalStateException e) {
+      errorExit(e.getMessage());
+    }
+  }
+  
+  public static MateUniverse current() {
+    if (Universe.getCurrent() == null) {
+      Universe.setCurrent(new MateUniverse());
+    }
+    return (MateUniverse) Universe.getCurrent();
+  }
+  
+  public boolean executingMeta() {
+    return executingMeta;
+  }
+  
+  public void enterMetaExecutionLevel() {
+    this.executingMeta = true;
+  }
+  
+  public void leaveMetaExecutionLevel() {
+    this.executingMeta = false;
+  }
+}
---
+++ MateExtensions/som/vm/constants/MateClasses.java
@@ -0,0 +1,20 @@
+package som.vm.constants;
+
+import som.vm.MateUniverse;
+import som.vmobjects.SClass;
+
+public class MateClasses extends Classes {
+  public static final SClass environmentMO;
+  public static final SClass operationalSemanticsMO;
+  public static final SClass messageMO;
+  public static final SClass ShapeClass;
+
+  static {
+    // Allocate the Metaclass classes
+    environmentMO = MateUniverse.newSystemClass();
+    operationalSemanticsMO = MateUniverse.newSystemClass();
+    messageMO = MateUniverse.newSystemClass();
+    ShapeClass = MateUniverse.newSystemClass();
+    //environment = Universe.newSystemClass();
+  }
+}
