5c4fd39a4717a7eccf7c2ba477826e731cbfb1c1
Guido Chari <charig@gmail.com>
2015-10-20 10:20:44 -0300

    Change MateExpection to be a subclass of the Truffle controlflowexception for performance reasons

--- MateExtensions/som/vm/MateSemanticsException.java
+++ MateExtensions/som/vm/MateSemanticsException.java
@@ -1,6 +1,8 @@
 package som.vm;
 
+import com.oracle.truffle.api.nodes.ControlFlowException;
 
-public class MateSemanticsException extends RuntimeException {
+
+public class MateSemanticsException extends ControlFlowException {
   private static final long serialVersionUID = 3450331007150656302L;
 }
