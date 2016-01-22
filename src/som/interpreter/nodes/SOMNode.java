0c63e100c8423ebaf368aa3e6ece3dba59f42cfb
Guido Chari <charig@gmail.com>
2016-01-22 18:21:46 -0300

    Correct indentation

--- src/som/interpreter/nodes/SOMNode.java
+++ src/som/interpreter/nodes/SOMNode.java
@@ -93,7 +93,7 @@ public abstract class SOMNode extends Node implements MateNode {
   public void wrapIntoMateNode(){
     Node replacement = this.asMateNode();
     if (replacement != null){
-        this.replace(replacement);
+      this.replace(replacement);
     }
   }
 }
