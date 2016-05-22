dc69c2e711b7a9db768e42c915e8c98f3ecdb512
Stefan Marr <git@stefan-marr.de>
2016-05-22 02:14:26 +0200

    Implemented missing `Integer // Double` specialization
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/arithmetic/DoubleDivPrim.java
+++ src/som/primitives/arithmetic/DoubleDivPrim.java
@@ -2,12 +2,12 @@ package som.primitives.arithmetic;
 
 import java.math.BigInteger;
 
-import som.vm.NotYetImplementedException;
-import som.vmobjects.SAbstractObject;
-
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
+
+import som.vm.NotYetImplementedException;
+import som.vmobjects.SAbstractObject;
 
 
 @GenerateNodeFactory
@@ -34,8 +34,7 @@ public abstract class DoubleDivPrim extends ArithmeticPrim {
   }
 
   @Specialization
-  public final SAbstractObject doLong(final long left, final double right) {
-    CompilerAsserts.neverPartOfCompilation("DoubleDiv101");
-    throw new NotYetImplementedException(); // TODO: need to implement the "/" case here directly... : return resendAsDouble("/", left, (SDouble) rightObj, frame.pack());
+  public final double doLong(final long left, final double right) {
+    return left / right;
   }
 }
