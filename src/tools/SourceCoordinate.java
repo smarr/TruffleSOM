df0bac9218daff7c8577dbbd6586b72f6144687f
Stefan Marr <git@stefan-marr.de>
2017-06-09 16:05:37 +0200

    Fix issue in Lexer by porting SOMns changes
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/tools/SourceCoordinate.java
@@ -0,0 +1,22 @@
+package tools;
+
+public class SourceCoordinate {
+  public final int startLine;
+  public final int startColumn;
+
+  public final transient int charIndex;
+
+  public SourceCoordinate(final int startLine, final int startColumn, final int charIndex) {
+    this.startLine = startLine;
+    this.startColumn = startColumn;
+    this.charIndex = charIndex;
+    assert startLine >= 0;
+    assert startColumn >= 0;
+    assert charIndex >= 0;
+  }
+
+  @Override
+  public String toString() {
+    return "SrcCoord(line: " + startLine + ", col: " + startColumn + ")";
+  }
+}
