f6ebbd3b2d7f2dc56e874b26260ed12635de8d0f
Stefan Marr <git@stefan-marr.de>
2020-03-30 14:11:08 +0100

    Rewrite Int[Down]ToDoMessageNode with new BlockDispatchNode
    Inspired by SOMns
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/interpreter/nodes/specialized/IntDownToDoMessageNode.java
+++ src/trufflesom/interpreter/nodes/specialized/IntDownToDoMessageNode.java
@@ -2,47 +2,33 @@ package trufflesom.interpreter.nodes.specialized;
 
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 
 import bd.primitives.Primitive;
 import trufflesom.interpreter.Invokable;
+import trufflesom.interpreter.nodes.dispatch.BlockDispatchNode;
+import trufflesom.interpreter.nodes.dispatch.BlockDispatchNodeGen;
 import trufflesom.interpreter.nodes.nary.TernaryExpressionNode;
-import trufflesom.interpreter.nodes.specialized.IntToDoMessageNode.ToDoSplzr;
 import trufflesom.vmobjects.SBlock;
-import trufflesom.vmobjects.SInvokable;
 
 
 @GenerateNodeFactory
-@Primitive(selector = "downTo:do:", noWrapper = true, disabled = true,
-    specializer = ToDoSplzr.class, inParser = false, requiresArguments = true)
+@Primitive(selector = "downTo:do:", noWrapper = true, disabled = true, inParser = false)
 public abstract class IntDownToDoMessageNode extends TernaryExpressionNode {
 
-  private final SInvokable      blockMethod;
-  @Child private DirectCallNode valueSend;
+  @Child private BlockDispatchNode blockNode = BlockDispatchNodeGen.create();
 
-  public IntDownToDoMessageNode(final Object[] args) {
-    blockMethod = ((SBlock) args[2]).getMethod();
-    valueSend = Truffle.getRuntime().createDirectCallNode(
-        blockMethod.getCallTarget());
-  }
-
-  protected final boolean isSameBlockLong(final SBlock block) {
-    return block.getMethod() == blockMethod;
-  }
-
-  @Specialization(guards = "isSameBlockLong(block)")
+  @Specialization
   public final long doIntDownToDo(final long receiver, final long limit, final SBlock block) {
     try {
       if (receiver >= limit) {
-        valueSend.call(new Object[] {block, receiver});
+        blockNode.executeDispatch(new Object[] {block, receiver});
       }
       for (long i = receiver - 1; i >= limit; i--) {
-        valueSend.call(new Object[] {block, i});
+        blockNode.executeDispatch(new Object[] {block, i});
       }
     } finally {
       if (CompilerDirectives.inInterpreter() && (receiver - limit) > 0) {
@@ -52,19 +38,15 @@ public abstract class IntDownToDoMessageNode extends TernaryExpressionNode {
     return receiver;
   }
 
-  protected final boolean isSameBlockDouble(final SBlock block) {
-    return block.getMethod() == blockMethod;
-  }
-
-  @Specialization(guards = "isSameBlockDouble(block)")
+  @Specialization
   public final long doIntDownToDo(final long receiver, final double limit,
       final SBlock block) {
     try {
       if (receiver >= limit) {
-        valueSend.call(new Object[] {block, receiver});
+        blockNode.executeDispatch(new Object[] {block, receiver});
       }
       for (long i = receiver - 1; i >= limit; i--) {
-        valueSend.call(new Object[] {block, i});
+        blockNode.executeDispatch(new Object[] {block, i});
       }
     } finally {
       if (CompilerDirectives.inInterpreter() && (receiver - (int) limit) > 0) {
--- src/trufflesom/interpreter/nodes/specialized/IntToDoMessageNode.java
+++ src/trufflesom/interpreter/nodes/specialized/IntToDoMessageNode.java
@@ -2,58 +2,26 @@ package trufflesom.interpreter.nodes.specialized;
 
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.CompilerDirectives;
-import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
-import com.oracle.truffle.api.dsl.NodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 
 import bd.primitives.Primitive;
-import bd.primitives.Specializer;
-import bd.settings.VmSettings;
 import trufflesom.interpreter.Invokable;
-import trufflesom.interpreter.nodes.ExpressionNode;
+import trufflesom.interpreter.nodes.dispatch.BlockDispatchNode;
+import trufflesom.interpreter.nodes.dispatch.BlockDispatchNodeGen;
 import trufflesom.interpreter.nodes.nary.TernaryExpressionNode;
-import trufflesom.interpreter.nodes.specialized.IntToDoMessageNode.ToDoSplzr;
-import trufflesom.vm.Universe;
 import trufflesom.vmobjects.SBlock;
-import trufflesom.vmobjects.SInvokable;
-import trufflesom.vmobjects.SSymbol;
 
 
 @GenerateNodeFactory
-@Primitive(selector = "to:do:", noWrapper = true, disabled = true,
-    specializer = ToDoSplzr.class, inParser = false, requiresArguments = true)
+@Primitive(selector = "to:do:", noWrapper = true, disabled = true, inParser = false)
 public abstract class IntToDoMessageNode extends TernaryExpressionNode {
 
-  public static class ToDoSplzr extends Specializer<Universe, ExpressionNode, SSymbol> {
-    public ToDoSplzr(final Primitive prim, final NodeFactory<ExpressionNode> fact) {
-      super(prim, fact);
-    }
+  @Child private BlockDispatchNode blockNode = BlockDispatchNodeGen.create();
 
-    @Override
-    public boolean matches(final Object[] args, final ExpressionNode[] argNodes) {
-      return !VmSettings.DYNAMIC_METRICS && args[0] instanceof Long &&
-          (args[1] instanceof Long || args[1] instanceof Double) &&
-          args[2] instanceof SBlock;
-    }
-  }
-
-  private final SInvokable      blockMethod;
-  @Child private DirectCallNode valueSend;
-
-  public IntToDoMessageNode(final Object[] args) {
-    blockMethod = ((SBlock) args[2]).getMethod();
-    valueSend = Truffle.getRuntime().createDirectCallNode(blockMethod.getCallTarget());
-  }
-
-  protected final boolean isSameBlockLong(final SBlock block) {
-    return block.getMethod() == blockMethod;
-  }
-
-  @Specialization(guards = "isSameBlockLong(block)")
+  @Specialization
   public final long doIntToDo(final long receiver, final long limit, final SBlock block) {
     try {
       doLooping(receiver, limit, block);
@@ -65,29 +33,18 @@ public abstract class IntToDoMessageNode extends TernaryExpressionNode {
     return receiver;
   }
 
-  protected final boolean isSameBlockDouble(final SBlock block) {
-    return block.getMethod() == blockMethod;
-  }
-
-  @Specialization(guards = "isSameBlockDouble(block)")
+  @Specialization
   public final long doIntToDo(final long receiver, final double dLimit, final SBlock block) {
     long limit = (long) dLimit;
-    try {
-      doLooping(receiver, limit, block);
-    } finally {
-      if (CompilerDirectives.inInterpreter()) {
-        reportLoopCount((int) limit - receiver);
-      }
-    }
-    return receiver;
+    return doIntToDo(receiver, limit, block);
   }
 
-  protected void doLooping(final long receiver, final long limit, final SBlock block) {
+  protected final void doLooping(final long receiver, final long limit, final SBlock block) {
     if (receiver <= limit) {
-      valueSend.call(new Object[] {block, receiver});
+      blockNode.executeDispatch(new Object[] {block, receiver});
     }
     for (long i = receiver + 1; i <= limit; i++) {
-      valueSend.call(new Object[] {block, i});
+      blockNode.executeDispatch(new Object[] {block, i});
     }
   }
 
