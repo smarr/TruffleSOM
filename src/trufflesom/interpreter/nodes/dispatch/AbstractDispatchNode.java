416f8d577cb8d893d41d606ba0127ece0c3e4ab7
Stefan Marr <git@stefan-marr.de>
2021-04-26 22:36:35 +0100

    Make dispatch chain walk non-recursive
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/interpreter/nodes/dispatch/AbstractDispatchNode.java
+++ src/trufflesom/interpreter/nodes/dispatch/AbstractDispatchNode.java
@@ -4,32 +4,66 @@ import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.DirectCallNode;
+import com.oracle.truffle.api.nodes.InvalidAssumptionException;
 import com.oracle.truffle.api.nodes.Node;
 
+import bd.primitives.nodes.PreevaluatedExpression;
 
-public abstract class AbstractDispatchNode extends Node implements DispatchChain {
+
+public abstract class AbstractDispatchNode extends Node implements PreevaluatedExpression {
   public static final int INLINE_CACHE_SIZE = 6;
 
-  public abstract Object executeDispatch(VirtualFrame frame, Object[] arguments);
+  @Override
+  public abstract Object doPreEvaluated(VirtualFrame frame, Object[] args);
 
-  public abstract static class AbstractCachedDispatchNode
-      extends AbstractDispatchNode {
+  public abstract boolean entryMatches(Object rcvr) throws InvalidAssumptionException;
 
-    @Child protected DirectCallNode       cachedMethod;
-    @Child protected AbstractDispatchNode nextInCache;
+  public abstract static class GuardedDispatchNode extends AbstractDispatchNode {
+    private final DispatchGuard guard;
 
-    public AbstractCachedDispatchNode(final CallTarget methodCallTarget,
-        final AbstractDispatchNode nextInCache) {
-      DirectCallNode cachedMethod =
-          Truffle.getRuntime().createDirectCallNode(methodCallTarget);
+    @Child public GuardedDispatchNode next;
 
-      this.cachedMethod = cachedMethod;
-      this.nextInCache = nextInCache;
+    protected GuardedDispatchNode(final DispatchGuard guard) {
+      this.guard = guard;
     }
 
     @Override
-    public final int lengthOfDispatchChain() {
-      return 1 + nextInCache.lengthOfDispatchChain();
+    public boolean entryMatches(final Object rcvr) throws InvalidAssumptionException {
+      return guard.entryMatches(rcvr);
+    }
+
+    public final <T extends Node> T insertHere(final T newChild) {
+      return super.insert(newChild);
+    }
+  }
+
+  public static final class CachedDispatchNode extends GuardedDispatchNode {
+
+    @Child protected DirectCallNode cachedMethod;
+
+    public CachedDispatchNode(final DispatchGuard guard, final CallTarget callTarget) {
+      super(guard);
+      cachedMethod = insert(Truffle.getRuntime().createDirectCallNode(callTarget));
+    }
+
+    @Override
+    public Object doPreEvaluated(final VirtualFrame frame, final Object[] arguments) {
+      return cachedMethod.call(arguments);
+    }
+  }
+
+  public static final class CachedExprNode extends GuardedDispatchNode {
+
+    @Child protected PreevaluatedExpression expr;
+
+    public CachedExprNode(final DispatchGuard guard, final PreevaluatedExpression expr) {
+      super(guard);
+      this.expr = expr;
+    }
+
+    @Override
+    public Object doPreEvaluated(final VirtualFrame frame, final Object[] arguments) {
+      return expr.doPreEvaluated(frame, arguments);
     }
   }
 }
--- src/trufflesom/interpreter/nodes/dispatch/GenericBlockDispatchNode.java
+++ src/trufflesom/interpreter/nodes/dispatch/GenericBlockDispatchNode.java
@@ -3,22 +3,26 @@ package trufflesom.interpreter.nodes.dispatch;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.IndirectCallNode;
+import com.oracle.truffle.api.nodes.InvalidAssumptionException;
 
 import trufflesom.vmobjects.SBlock;
 
 
 public final class GenericBlockDispatchNode extends AbstractDispatchNode {
-  @Child private IndirectCallNode call = Truffle.getRuntime().createIndirectCallNode();
+  @Child private IndirectCallNode call;
+
+  public GenericBlockDispatchNode() {
+    call = insert(Truffle.getRuntime().createIndirectCallNode());
+  }
 
   @Override
-  public Object executeDispatch(final VirtualFrame frame,
-      final Object[] arguments) {
+  public Object doPreEvaluated(final VirtualFrame frame, final Object[] arguments) {
     SBlock rcvr = (SBlock) arguments[0];
     return call.call(rcvr.getMethod().getCallTarget(), arguments);
   }
 
   @Override
-  public int lengthOfDispatchChain() {
-    return 1000;
+  public boolean entryMatches(final Object rcvr) throws InvalidAssumptionException {
+    return true;
   }
 }
