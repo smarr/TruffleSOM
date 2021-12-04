37be5f151e0dd641589887b37c6450f3ffa50ea2
Stefan Marr <git@stefan-marr.de>
2021-12-04 23:28:05 +0000

    Don't use block node in #downTo:do:
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/interpreter/nodes/specialized/IntDownToDoMessageNode.java
+++ src/trufflesom/interpreter/nodes/specialized/IntDownToDoMessageNode.java
@@ -1,37 +1,49 @@
 package trufflesom.interpreter.nodes.specialized;
 
+import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.DirectCallNode;
+import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RootNode;
 
 import bd.primitives.Primitive;
 import trufflesom.interpreter.Invokable;
-import trufflesom.interpreter.nodes.dispatch.BlockDispatchNode;
-import trufflesom.interpreter.nodes.dispatch.BlockDispatchNodeGen;
 import trufflesom.interpreter.nodes.nary.TernaryExpressionNode;
 import trufflesom.vm.SymbolTable;
 import trufflesom.vmobjects.SBlock;
+import trufflesom.vmobjects.SInvokable;
 
 
 @GenerateNodeFactory
 @Primitive(selector = "downTo:do:", disabled = true, inParser = false)
 public abstract class IntDownToDoMessageNode extends TernaryExpressionNode {
 
-  @Child private BlockDispatchNode blockNode = BlockDispatchNodeGen.create();
+  protected static final DirectCallNode createCallNode(final CallTarget ct) {
+    return Truffle.getRuntime().createDirectCallNode(ct);
+  }
 
-  @Specialization
-  public final long doIntDownToDo(final long receiver, final long limit, final SBlock block) {
+  protected static final IndirectCallNode createIndirectCall() {
+    return Truffle.getRuntime().createIndirectCallNode();
+  }
+
+  @Specialization(guards = {"block.getMethod() == cachedMethod"})
+  public final long doIntCached(final long receiver, final long limit, final SBlock block,
+      @Cached("block.getMethod()") final SInvokable cachedMethod,
+      @Cached("createCallNode(cachedMethod.getCallTarget())") final DirectCallNode callNode) {
     try {
       if (receiver >= limit) {
-        blockNode.executeDispatch(new Object[] {block, receiver});
+        callNode.call(new Object[] {block, receiver});
       }
       for (long i = receiver - 1; i >= limit; i--) {
-        blockNode.executeDispatch(new Object[] {block, i});
+        callNode.call(new Object[] {block, i});
       }
     } finally {
       if (CompilerDirectives.inInterpreter() && (receiver - limit) > 0) {
@@ -41,15 +53,35 @@ public abstract class IntDownToDoMessageNode extends TernaryExpressionNode {
     return receiver;
   }
 
-  @Specialization
-  public final long doIntDownToDo(final long receiver, final double limit,
-      final SBlock block) {
+  @Specialization(replaces = "doIntCached")
+  public final long doIntUncached(final long receiver, final long limit, final SBlock block,
+      @Cached("createIndirectCall()") final IndirectCallNode callNode) {
+    CallTarget ct = block.getMethod().getCallTarget();
     try {
       if (receiver >= limit) {
-        blockNode.executeDispatch(new Object[] {block, receiver});
+        callNode.call(ct, new Object[] {block, receiver});
       }
       for (long i = receiver - 1; i >= limit; i--) {
-        blockNode.executeDispatch(new Object[] {block, i});
+        callNode.call(ct, new Object[] {block, i});
+      }
+    } finally {
+      if (CompilerDirectives.inInterpreter() && (receiver - limit) > 0) {
+        reportLoopCount(receiver - limit);
+      }
+    }
+    return receiver;
+  }
+
+  @Specialization(guards = {"block.getMethod() == cachedMethod"})
+  public final long doDoubleCached(final long receiver, final double limit, final SBlock block,
+      @Cached("block.getMethod()") final SInvokable cachedMethod,
+      @Cached("createCallNode(cachedMethod.getCallTarget())") final DirectCallNode callNode) {
+    try {
+      if (receiver >= limit) {
+        callNode.call(new Object[] {block, receiver});
+      }
+      for (long i = receiver - 1; i >= limit; i--) {
+        callNode.call(new Object[] {block, i});
       }
     } finally {
       if (CompilerDirectives.inInterpreter() && (receiver - (int) limit) > 0) {
@@ -59,16 +91,18 @@ public abstract class IntDownToDoMessageNode extends TernaryExpressionNode {
     return receiver;
   }
 
-  @Specialization
-  public final double doDoubleDownToDo(final double receiver, final double limit,
-      final SBlock block) {
+  @Specialization(replaces = "doDoubleCached")
+  public final double doDoubleUncached(final double receiver, final double limit,
+      final SBlock block,
+      @Cached("createIndirectCall()") final IndirectCallNode callNode) {
+    CallTarget ct = block.getMethod().getCallTarget();
     try {
       if (receiver >= limit) {
-        blockNode.executeDispatch(new Object[] {block, receiver});
+        callNode.call(ct, new Object[] {block, receiver});
       }
       double i = receiver - 1.0;
       while (i >= limit) {
-        blockNode.executeDispatch(new Object[] {block, i});
+        callNode.call(ct, new Object[] {block, i});
         i -= 1.0;
       }
     } finally {
