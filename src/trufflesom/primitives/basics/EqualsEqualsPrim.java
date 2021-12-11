24e1aeeb6b5518350b26f98de280536b0c0f6e03
Stefan Marr <git@stefan-marr.de>
2021-12-11 12:34:59 +0000

    Minimize Object>>#== implementation
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/primitives/basics/EqualsEqualsPrim.java
+++ src/trufflesom/primitives/basics/EqualsEqualsPrim.java
@@ -1,36 +1,19 @@
 package trufflesom.primitives.basics;
 
-import java.math.BigInteger;
-
+import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 
 import bd.primitives.Primitive;
-import trufflesom.interpreter.nodes.nary.BinaryMsgExprNode;
-import trufflesom.vm.SymbolTable;
-import trufflesom.vmobjects.SArray;
-import trufflesom.vmobjects.SBlock;
-import trufflesom.vmobjects.SInvokable;
-import trufflesom.vmobjects.SObject;
-import trufflesom.vmobjects.SSymbol;
+import trufflesom.interpreter.nodes.nary.BinaryExpressionNode;
 
 
 @GenerateNodeFactory
 @Primitive(className = "Object", primitive = "==", selector = "==")
-public abstract class EqualsEqualsPrim extends BinaryMsgExprNode {
-  @Override
-  public SSymbol getSelector() {
-    return SymbolTable.symbolFor("==");
-  }
-
+public abstract class EqualsEqualsPrim extends BinaryExpressionNode {
   @Specialization
   public final boolean doBoolean(final boolean left, final boolean right) {
     return left == right;
-  }
-
-  @Specialization
-  public final boolean doBoolean(final boolean left, final SObject right) {
-    return false;
   }
 
   @Specialization
@@ -39,87 +22,12 @@ public abstract class EqualsEqualsPrim extends BinaryMsgExprNode {
   }
 
   @Specialization
-  public final boolean doBigInteger(final BigInteger left, final BigInteger right) {
-    return left == right;
-  }
-
-  @Specialization
-  public final boolean doString(final String left, final String right) {
-    return left == right;
-  }
-
-  @Specialization
   public final boolean doDouble(final double left, final double right) {
     return left == right;
   }
 
-  @Specialization
-  public final boolean doSBlock(final SBlock left, final Object right) {
-    return left == right;
-  }
-
-  @Specialization
-  public final boolean doArray(final SArray left, final Object right) {
-    return left == right;
-  }
-
-  @Specialization
-  public final boolean doSMethod(final SInvokable left, final Object right) {
-    return left == right;
-  }
-
-  @Specialization
-  public final boolean doSSymbol(final SSymbol left, final Object right) {
-    return left == right;
-  }
-
-  @Specialization
-  public final boolean doSObject(final SObject left, final Object right) {
-    return left == right;
-  }
-
-  @Specialization
-  public final boolean doLong(final long left, final double right) {
-    return false;
-  }
-
-  @Specialization
-  public final boolean doBigInteger(final BigInteger left, final long right) {
-    return false;
-  }
-
-  @Specialization
-  public final boolean doLong(final long left, final BigInteger right) {
-    return false;
-  }
-
-  @Specialization
-  public final boolean doDouble(final double left, final long right) {
-    return false;
-  }
-
-  @Specialization
-  public final boolean doLong(final long left, final String right) {
-    return false;
-  }
-
-  @Specialization
-  public final boolean doLong(final long left, final SObject right) {
-    return false;
-  }
-
-  @Specialization
-  public final boolean doString(final String receiver, final long argument) {
-    return false;
-  }
-
-  @Specialization
-  public final boolean doString(final String receiver, final SSymbol argument) {
-    return false;
-  }
-
-  @Specialization
-  public final boolean doString(final String receiver, final SObject argument) {
-    return false;
+  @Fallback
+  public final boolean fallback(final Object receiver, final Object argument) {
+    return receiver == argument;
   }
 }
