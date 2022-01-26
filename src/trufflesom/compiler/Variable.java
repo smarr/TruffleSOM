f15d40b0f63b3c23655d522a8b0a15cf0e856f2c
Stefan Marr <git@stefan-marr.de>
2022-01-26 12:22:17 +0000

    Add Inc[Non]LocalVariableNode
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/compiler/Variable.java
+++ src/trufflesom/compiler/Variable.java
@@ -22,16 +22,21 @@ import trufflesom.interpreter.nodes.ArgumentReadNode.LocalArgumentWriteNode;
 import trufflesom.interpreter.nodes.ArgumentReadNode.NonLocalArgumentReadNode;
 import trufflesom.interpreter.nodes.ArgumentReadNode.NonLocalArgumentWriteNode;
 import trufflesom.interpreter.nodes.ExpressionNode;
+import trufflesom.interpreter.nodes.LocalVariableNode.LocalVariableReadNode;
 import trufflesom.interpreter.nodes.LocalVariableNodeFactory.LocalVariableReadNodeGen;
 import trufflesom.interpreter.nodes.LocalVariableNodeFactory.LocalVariableWriteNodeGen;
+import trufflesom.interpreter.nodes.NonLocalVariableNode.NonLocalVariableReadNode;
 import trufflesom.interpreter.nodes.NonLocalVariableNodeFactory.NonLocalVariableReadNodeGen;
 import trufflesom.interpreter.nodes.NonLocalVariableNodeFactory.NonLocalVariableWriteNodeGen;
-import trufflesom.interpreter.supernodes.LocalVariableIncNodeGen;
+import trufflesom.interpreter.supernodes.IncLocalVariableNodeGen;
+import trufflesom.interpreter.supernodes.IncNonLocalVariableNodeGen;
+import trufflesom.interpreter.supernodes.IntIncLocalVariableNodeGen;
+import trufflesom.interpreter.supernodes.IntIncNonLocalVariableNodeGen;
 import trufflesom.interpreter.supernodes.LocalVariableReadSquareWriteNodeGen;
 import trufflesom.interpreter.supernodes.LocalVariableSquareNodeGen;
-import trufflesom.interpreter.supernodes.NonLocalVariableIncNodeGen;
 import trufflesom.interpreter.supernodes.NonLocalVariableReadSquareWriteNodeGen;
 import trufflesom.interpreter.supernodes.NonLocalVariableSquareNodeGen;
+import trufflesom.primitives.arithmetic.AdditionPrim;
 import trufflesom.vm.NotYetImplementedException;
 import trufflesom.vmobjects.SSymbol;
 
@@ -204,17 +209,17 @@ public abstract class Variable implements bdt.inlining.Variable<ExpressionNode> 
     @Override
     public ExpressionNode getIncNode(final int contextLevel, final long incValue,
         final long coord) {
-      transferToInterpreterAndInvalidate("Variable.getReadNode");
+      transferToInterpreterAndInvalidate();
       if (contextLevel > 0) {
-        return NonLocalVariableIncNodeGen.create(contextLevel, this, incValue)
-                                         .initialize(coord);
+        return IntIncNonLocalVariableNodeGen.create(contextLevel, this, incValue)
+                                            .initialize(coord);
       }
-      return LocalVariableIncNodeGen.create(this, incValue).initialize(coord);
+      return IntIncLocalVariableNodeGen.create(this, incValue).initialize(coord);
     }
 
     @Override
     public ExpressionNode getSquareNode(final int contextLevel, final long coord) {
-      transferToInterpreterAndInvalidate("Variable.getReadNode");
+      transferToInterpreterAndInvalidate();
       if (contextLevel > 0) {
         return NonLocalVariableSquareNodeGen.create(contextLevel, this).initialize(coord);
       }
@@ -249,10 +254,48 @@ public abstract class Variable implements bdt.inlining.Variable<ExpressionNode> 
     public ExpressionNode getWriteNode(final int contextLevel,
         final ExpressionNode valueExpr, final long coord) {
       transferToInterpreterAndInvalidate();
+
       if (contextLevel > 0) {
+        if (valueExpr instanceof AdditionPrim) {
+          AdditionPrim add = (AdditionPrim) valueExpr;
+          ExpressionNode rcvr = add.getReceiver();
+          ExpressionNode arg = add.getArgument();
+
+          if (rcvr instanceof NonLocalVariableReadNode
+              && ((NonLocalVariableReadNode) rcvr).getLocal() == this) {
+            return IncNonLocalVariableNodeGen.create(contextLevel, this, arg)
+                                             .initialize(coord);
+          }
+
+          if (arg instanceof NonLocalVariableReadNode
+              && ((NonLocalVariableReadNode) arg).getLocal() == this) {
+            return IncNonLocalVariableNodeGen.create(contextLevel, this, rcvr)
+                                             .initialize(coord);
+          }
+        }
+
         return NonLocalVariableWriteNodeGen.create(contextLevel, this, valueExpr)
                                            .initialize(coord);
       }
+
+      if (valueExpr instanceof AdditionPrim) {
+        AdditionPrim add = (AdditionPrim) valueExpr;
+        ExpressionNode rcvr = add.getReceiver();
+        ExpressionNode arg = add.getArgument();
+
+        if (rcvr instanceof LocalVariableReadNode
+            && ((LocalVariableReadNode) rcvr).getLocal() == this) {
+          return IncLocalVariableNodeGen.create(this, arg)
+                                        .initialize(coord);
+        }
+
+        if (arg instanceof LocalVariableReadNode
+            && ((LocalVariableReadNode) arg).getLocal() == this) {
+          return IncLocalVariableNodeGen.create(this, rcvr)
+                                        .initialize(coord);
+        }
+      }
+
       return LocalVariableWriteNodeGen.create(this, valueExpr).initialize(coord);
     }
 
---
+++ src/trufflesom/interpreter/supernodes/IncNonLocalVariableNode.java
@@ -0,0 +1,101 @@
+package trufflesom.interpreter.supernodes;
+
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Bind;
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.FrameSlotTypeException;
+import com.oracle.truffle.api.frame.MaterializedFrame;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.Node;
+
+import bd.inlining.ScopeAdaptationVisitor;
+import bd.inlining.ScopeAdaptationVisitor.ScopeElement;
+import trufflesom.compiler.Variable.Local;
+import trufflesom.interpreter.nodes.ExpressionNode;
+import trufflesom.interpreter.nodes.NonLocalVariableNode;
+import trufflesom.primitives.arithmetic.AdditionPrim;
+import trufflesom.primitives.arithmetic.AdditionPrimFactory;
+
+
+@NodeChild(value = "value", type = ExpressionNode.class)
+public abstract class IncNonLocalVariableNode extends NonLocalVariableNode {
+
+  protected IncNonLocalVariableNode(final int contextLevel, final Local local) {
+    super(contextLevel, local);
+  }
+
+  public abstract ExpressionNode getValue();
+
+  @Specialization(guards = "ctx.isLong(slot)", rewriteOn = {FrameSlotTypeException.class})
+  public final long doLong(final VirtualFrame frame, final long value,
+      @Bind("determineContext(frame)") final MaterializedFrame ctx)
+      throws FrameSlotTypeException {
+    long current = ctx.getLong(slot);
+    long result = Math.addExact(current, value);
+    ctx.setLong(slot, result);
+    return result;
+  }
+
+  @Specialization(guards = "ctx.isDouble(slot)", rewriteOn = {FrameSlotTypeException.class})
+  public final double doDouble(final VirtualFrame frame, final double value,
+      @Bind("determineContext(frame)") final MaterializedFrame ctx)
+      throws FrameSlotTypeException {
+    double current = ctx.getDouble(slot);
+    double result = current + value;
+    ctx.setDouble(slot, result);
+    return result;
+  }
+
+  @Specialization(guards = "ctx.isObject(slot)", rewriteOn = {FrameSlotTypeException.class})
+  public final Object doString(final VirtualFrame frame, final String value,
+      @Bind("determineContext(frame)") final MaterializedFrame ctx)
+      throws FrameSlotTypeException {
+    String current = (String) ctx.getObject(slot);
+    String result = concat(current, value);
+    ctx.setObject(slot, result);
+    return result;
+  }
+
+  @TruffleBoundary
+  private static String concat(final String a, final String b) {
+    return a.concat(b);
+  }
+
+  @Fallback
+  public final Object fallback(final VirtualFrame frame, final Object value) {
+    MaterializedFrame ctx = determineContext(frame);
+    CompilerDirectives.transferToInterpreterAndInvalidate();
+
+    AdditionPrim add =
+        AdditionPrimFactory.create(local.getReadNode(contextLevel, sourceCoord), getValue());
+    add.initialize(sourceCoord);
+
+    replace(local.getWriteNode(contextLevel, add, sourceCoord)).adoptChildren();
+
+    Object preIncValue = ctx.getValue(slot);
+    Object result = add.executeEvaluated(null, preIncValue, value);
+    ctx.setObject(slot, result);
+    return result;
+  }
+
+  @Override
+  public void replaceAfterScopeChange(final ScopeAdaptationVisitor inliner) {
+    ScopeElement<? extends Node> se = inliner.getAdaptedVar(local);
+    if (se.var != local || se.contextLevel < contextLevel) {
+      ExpressionNode node;
+      if (se.contextLevel == 0) {
+        node = IncLocalVariableNodeGen.create((Local) se.var, getValue());
+      } else {
+        node = IncNonLocalVariableNodeGen.create(se.contextLevel, (Local) se.var, getValue());
+      }
+      node.initialize(sourceCoord);
+
+      replace(node);
+    } else {
+      assert contextLevel == se.contextLevel;
+    }
+  }
+}
