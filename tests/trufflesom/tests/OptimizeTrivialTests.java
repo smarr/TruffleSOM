d1acd3e9050f35b4b05941c4fd98eb259c797c31
Stefan Marr <git@stefan-marr.de>
2022-02-13 20:36:22 +0000

    Add tests for the new dispatch chain entries
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/trufflesom/tests/OptimizeTrivialTests.java
+++ tests/trufflesom/tests/OptimizeTrivialTests.java
@@ -10,6 +10,7 @@ import static trufflesom.vm.SymbolTable.symbolFor;
 
 import org.junit.Test;
 
+import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.source.Source;
 
 import bd.basic.ProgramDefinitionError;
@@ -17,10 +18,12 @@ import bd.primitives.nodes.PreevaluatedExpression;
 import bd.source.SourceCoordinate;
 import trufflesom.compiler.ClassGenerationContext;
 import trufflesom.compiler.MethodGenerationContext;
+import trufflesom.compiler.Parser.ParseError;
 import trufflesom.compiler.ParserAst;
 import trufflesom.compiler.ParserBc;
 import trufflesom.compiler.bc.BytecodeMethodGenContext;
 import trufflesom.interpreter.Method;
+import trufflesom.interpreter.Primitive;
 import trufflesom.interpreter.SomLanguage;
 import trufflesom.interpreter.nodes.ExpressionNode;
 import trufflesom.interpreter.nodes.FieldNode.FieldReadNode;
@@ -29,11 +32,27 @@ import trufflesom.interpreter.nodes.GlobalNode;
 import trufflesom.interpreter.nodes.GlobalNode.FalseGlobalNode;
 import trufflesom.interpreter.nodes.GlobalNode.NilGlobalNode;
 import trufflesom.interpreter.nodes.GlobalNode.TrueGlobalNode;
+import trufflesom.interpreter.nodes.dispatch.AbstractDispatchNode;
+import trufflesom.interpreter.nodes.dispatch.CachedDispatchNode;
+import trufflesom.interpreter.nodes.dispatch.CachedExprNode;
+import trufflesom.interpreter.nodes.dispatch.CachedFieldRead;
+import trufflesom.interpreter.nodes.dispatch.CachedFieldWriteAndSelf;
+import trufflesom.interpreter.nodes.dispatch.CachedLiteralNode;
+import trufflesom.interpreter.nodes.dispatch.CachedNewObject;
+import trufflesom.interpreter.nodes.dispatch.UninitializedDispatchNode;
 import trufflesom.interpreter.nodes.literals.DoubleLiteralNode;
 import trufflesom.interpreter.nodes.literals.GenericLiteralNode;
 import trufflesom.interpreter.nodes.literals.IntegerLiteralNode;
+import trufflesom.interpreter.objectstorage.ObjectLayout;
+import trufflesom.primitives.Primitives;
+import trufflesom.primitives.basics.NewObjectPrimFactory;
+import trufflesom.vm.Universe;
 import trufflesom.vm.VmSettings;
+import trufflesom.vmobjects.SClass;
 import trufflesom.vmobjects.SInvokable;
+import trufflesom.vmobjects.SInvokable.SPrimitive;
+import trufflesom.vmobjects.SObject;
+import trufflesom.vmobjects.SSymbol;
 
 
 public class OptimizeTrivialTests extends TruffleTestSetup {
@@ -41,7 +60,7 @@ public class OptimizeTrivialTests extends TruffleTestSetup {
   protected MethodGenerationContext mgenc;
   protected MethodGenerationContext bgenc;
 
-  private Method parseMethod(final String source) {
+  private SInvokable parseMethodToSInvokable(final String source) {
     Source s = SomLanguage.getSyntheticSource(source, "test");
 
     cgenc = new ClassGenerationContext(s, null);
@@ -57,22 +76,39 @@ public class OptimizeTrivialTests extends TruffleTestSetup {
 
     long coord = SourceCoordinate.create(0, 10);
 
-    SInvokable ivkbl;
-
     try {
       if (VmSettings.UseAstInterp) {
         ParserAst parser = new ParserAst(source, s, null);
         ExpressionNode body = parser.method(mgenc);
-        ivkbl = mgenc.assemble(body, coord);
+        return mgenc.assemble(body, coord);
       } else {
         ParserBc parser = new ParserBc(source, s, probe);
         parser.method((BytecodeMethodGenContext) mgenc);
-        ivkbl = mgenc.assemble(null, coord);
+        return mgenc.assemble(null, coord);
       }
-      return (Method) ivkbl.getInvokable();
     } catch (ProgramDefinitionError e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private Method parseMethod(final String source) {
+    SInvokable ivkbl = parseMethodToSInvokable(source);
+    return (Method) ivkbl.getInvokable();
+  }
+
+  private SClass parseMethodAndConstructClass(final String source,
+      final SClass superClass) {
+    SInvokable m = parseMethodToSInvokable(source);
+    if (superClass != null) {
+      cgenc.setSuperClass(superClass);
+    }
+
+    try {
+      cgenc.addInstanceMethod(m, null);
+    } catch (ParseError e) {
+      throw new RuntimeException(e);
+    }
+    return cgenc.assemble();
   }
 
   private Method parseBlock(final String source) {
@@ -337,5 +373,149 @@ public class OptimizeTrivialTests extends TruffleTestSetup {
   public void testUnknownGlobalInBlock() {
     Method m = parseBlock("[ UnknownGlobalSSSS ]");
     assertFalse(m.isTrivial());
+  }
+
+  private void trivialMethod(final String methodName, final String methodBody,
+      final Class<?> expectedClass) {
+    trivialMethod(methodName, methodName, methodBody, null, expectedClass);
+  }
+
+  private void trivialMethod(final String methodName, final String methodSig,
+      final String methodBody, final SClass superClass, final Class<?> expectedClass) {
+    int numFields = fieldNames.size();
+    SClass clazz =
+        parseMethodAndConstructClass(methodSig + " = ( " + methodBody + " )", superClass);
+    SObject object = new SObject(clazz, new ObjectLayout(numFields, clazz));
+    AbstractDispatchNode dispatch =
+        UninitializedDispatchNode.createDispatch(object, symbolFor(methodName),
+            null);
+    assertThat(dispatch, instanceOf(expectedClass));
+  }
+
+  @Test
+  public void testDispatchForReturnInteger() {
+    trivialMethod("test", "^ 1", CachedLiteralNode.class);
+  }
+
+  @Test
+  public void testDispatchForReturnDouble() {
+    trivialMethod("test", "^ -4.5", CachedLiteralNode.class);
+  }
+
+  @Test
+  public void testDispatchForReturnTrue() {
+    trivialMethod("test", "^ true", CachedLiteralNode.class);
+  }
+
+  @Test
+  public void testDispatchForReturnFalse() {
+    trivialMethod("test", "^ false", CachedLiteralNode.class);
+  }
+
+  @Test
+  public void testDispatchForReturnNil() {
+    trivialMethod("test", "^ nil", CachedLiteralNode.class);
+  }
+
+  @Test
+  public void testDispatchForReturnString() {
+    trivialMethod("test", "^ 'str'", CachedLiteralNode.class);
+  }
+
+  @Test
+  public void testDispatchForReturnSymbol() {
+    trivialMethod("test", "^ #sym", CachedLiteralNode.class);
+  }
+
+  @Test
+  public void testDispatchForReturnUnknownGlobal() {
+    trivialMethod("test", "^ UnknownGlobal", CachedExprNode.class);
+  }
+
+  @Test
+  public void testDispatchForReturnBlock() {
+    trivialMethod("test", "^ []", CachedDispatchNode.class);
+  }
+
+  @Test
+  public void testDispatchForFieldRead() {
+    addField("field");
+    trivialMethod("test", "^ field", CachedFieldRead.class);
+  }
+
+  @Test
+  public void testDispatchForFieldWrite() {
+    SClass objClazz = constructDummyObjectClass();
+
+    addField("field");
+    trivialMethod("test:", "test: arg", "field := arg", objClazz,
+        CachedFieldWriteAndSelf.class);
+  }
+
+  @Test
+  public void testDispatchNew() {
+    SClass objClazz = constructDummyObjectClass();
+    SClass clazz = parseMethodAndConstructClass("test = ()", objClazz);
+    AbstractDispatchNode dispatch =
+        UninitializedDispatchNode.createDispatch(clazz, symbolFor("new"), null);
+    assertThat(dispatch, instanceOf(CachedNewObject.class));
+  }
+
+  private SClass constructDummyObjectClass() {
+    Source s = SomLanguage.getSyntheticSource("dummy-content", "test");
+    ClassGenerationContext objCgenc = new ClassGenerationContext(s, null);
+    objCgenc.setName(symbolFor("Object"));
+
+    try {
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("="), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("=="), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("~="), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("class"), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("halt"), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("hashcode"), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("objectSize"), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("instVarNamed:"), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("instVarAt:"), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("instVarAt:put:"), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("perform:"), s, 1, probe), null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("perform:withArguments:"), s, 1, probe),
+          null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("perform:withArguments:inSuperclass:"),
+              s, 1, probe),
+          null);
+      objCgenc.addInstanceMethod(
+          Primitives.constructEmptyPrimitive(symbolFor("perform:inSuperclass:"), s, 1, probe),
+          null);
+
+      objCgenc.addClassMethod(constructDummyNewPrim(s), null);
+    } catch (ParseError e) {
+      throw new RuntimeException(e);
+    }
+
+    SClass objClazz = objCgenc.assemble();
+    Universe.loadPrimitives(objClazz, true);
+
+    return objClazz;
+  }
+
+  private SPrimitive constructDummyNewPrim(final Source s) {
+    ExpressionNode newPrim = NewObjectPrimFactory.create(null);
+    SSymbol symNew = symbolFor("new");
+    Primitive primMethodNode = new Primitive(
+        "new", s, 1, newPrim, new FrameDescriptor(), newPrim);
+    return new SPrimitive(symNew, primMethodNode);
   }
 }
