274c0e668160469d239bec9940ed3c357297b0d1
Stefan Marr <git@stefan-marr.de>
2023-04-24 23:43:44 +0100

    Added more tests for the string literal equality supernode

--- tests/trufflesom/supernodes/StringEqualsTests.java
+++ tests/trufflesom/supernodes/StringEqualsTests.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 
 import trufflesom.interpreter.nodes.ExpressionNode;
 import trufflesom.interpreter.nodes.SequenceNode;
+import trufflesom.interpreter.nodes.literals.BlockNode;
 import trufflesom.interpreter.supernodes.LocalFieldStringEqualsNode;
+import trufflesom.interpreter.supernodes.NonLocalFieldStringEqualsNode;
 import trufflesom.interpreter.supernodes.StringEqualsNode;
 import trufflesom.tests.AstTestSetup;
 
@@ -25,6 +27,19 @@ public class StringEqualsTests extends AstTestSetup {
     return (T) testExpr;
   }
 
+  @SuppressWarnings("unchecked")
+  private <T> T assertInBlock(final String test, final Class<T> expectedNode) {
+    addField("field");
+    SequenceNode seq = (SequenceNode) parseMethod(
+        "test: arg = ( | var | \n" + test + " )");
+
+    BlockNode block = (BlockNode) read(seq, "expressions", 0);
+    ExpressionNode testExpr =
+        read(block.getMethod().getInvokable(), "body", ExpressionNode.class);
+    assertThat(testExpr, instanceOf(expectedNode));
+    return (T) testExpr;
+  }
+
   @Test
   public void testStringEqual() {
     assertThatMainNodeIs("field = 'str'", LocalFieldStringEqualsNode.class);
@@ -36,5 +51,24 @@ public class StringEqualsTests extends AstTestSetup {
     assertThatMainNodeIs("'str' = arg", StringEqualsNode.class);
     assertThatMainNodeIs("'str' = var", StringEqualsNode.class);
     assertThatMainNodeIs("'str' = ('s' + 'dd')", StringEqualsNode.class);
+  }
+
+  @Test
+  public void testStringEqualInBlock() {
+    assertInBlock("[ field = 'str' ] ", NonLocalFieldStringEqualsNode.class);
+    assertInBlock("[ arg = 'str' ]", StringEqualsNode.class);
+    assertInBlock("[ var = 'str' ]", StringEqualsNode.class);
+
+    assertInBlock("[:a | a = 'str' ]", StringEqualsNode.class);
+    assertInBlock("[ | v | v = 'str' ]", StringEqualsNode.class);
+
+    assertInBlock("[ ('s' + 'dd') = 'str' ]", StringEqualsNode.class);
+
+    assertInBlock("[ 'str' = field ]", NonLocalFieldStringEqualsNode.class);
+    assertInBlock("[ 'str' = arg ]", StringEqualsNode.class);
+    assertInBlock("[ 'str' = var ]", StringEqualsNode.class);
+    assertInBlock("[:a | 'str' = a ]", StringEqualsNode.class);
+    assertInBlock("[ | v|  'str' = v ]", StringEqualsNode.class);
+    assertInBlock("[ 'str' = ('s' + 'dd') ]", StringEqualsNode.class);
   }
 }
