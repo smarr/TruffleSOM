8e79d720de09eeaa28e96cbd958953ff231b2fc2
Stefan Marr <git@stefan-marr.de>
2023-07-25 10:33:50 +0100

    Added mx_trufflesom.py to automatically make sure submodules are loaded

---
+++ mx.trufflesom/mx_trufflesom.py
@@ -0,0 +1,8 @@
+import os
+from mx import GitConfig
+
+
+# ensure the core-lib is present
+if not os.path.exists('core-lib/.git'):
+  git = GitConfig()
+  git.run(['git', 'submodule', 'update', '--init', '--recursive'])
