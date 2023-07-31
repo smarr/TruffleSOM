f16725360af0ba8678b37d47229c05c10523a1ee
Stefan Marr <git@stefan-marr.de>
2023-07-31 18:12:01 +0100

    Format suite.py, too

--- mx.trufflesom/lint.sh
+++ mx.trufflesom/lint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-python -m black ${SCRIPT_DIR}/mx_trufflesom.py
+python -m black ${SCRIPT_DIR}/mx_trufflesom.py ${SCRIPT_DIR}/suite.py
 python -m pylint ${SCRIPT_DIR}/mx_trufflesom.py
--- mx.trufflesom/suite.py
+++ mx.trufflesom/suite.py
@@ -12,45 +12,42 @@ suite = {
         "write": "git@github.com:SOM-st/TruffleSOM.git",
     },
     "imports": {
-        "suites": [{
-            "name": "truffle",
-            "subdir": True,
-            "version": "ef253f24988ea66c63fe92cda4a8a555bc02fe38",
-            "urls": [{
-                "url": "https://github.com/oracle/graal",
-                "kind": "git"
-            }],
-        }]
+        "suites": [
+            {
+                "name": "truffle",
+                "subdir": True,
+                "version": "ef253f24988ea66c63fe92cda4a8a555bc02fe38",
+                "urls": [{"url": "https://github.com/oracle/graal", "kind": "git"}],
+            },
+        ]
     },
     "libraries": {
-        "CHECKSTYLE_10.9.3" : {
-            "urls" : [
+        "CHECKSTYLE_10.9.3": {
+            "urls": [
                 "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.9.3/checkstyle-10.9.3-all.jar"
             ],
             "sha1": "5bd65d2d7e0eefe73782c072db2066f6832d5b43",
-            "licence" : "LGPLv21"
+            "licence": "LGPLv21",
         },
-        "LABS_JDK" : {
+        "LABS_JDK": {
             "id": "labsjdk-ce-20",
-             # I am just using the suite.py to store the info
-             # so but manage it in mx_trufflesom.py
-            "path": "."
-        }
+            # I am just using the suite.py to store the info
+            # so but manage it in mx_trufflesom.py
+            "path": ".",
+        },
     },
     "projects": {
         "trufflesom": {
             "subDir": "src",
             "sourceDirs": ["src"],
-            "dependencies": [
-                "truffle:TRUFFLE_API"
-            ],
+            "dependencies": ["truffle:TRUFFLE_API"],
             "requires": [
                 "java.logging",
                 "java.management",
                 "jdk.management",
-                "jdk.unsupported" # sun.misc.Unsafe
+                "jdk.unsupported",  # sun.misc.Unsafe
             ],
-            "requiresConcealed" : {
+            "requiresConcealed": {
                 "java.base": ["jdk.internal.module"],
             },
             "checkstyleVersion": "10.9.3",
@@ -65,11 +62,7 @@ suite = {
             "requires": [
                 "java.logging",
             ],
-            "dependencies": [
-                "truffle:TRUFFLE_API",
-                "TRUFFLESOM",
-                "mx:JUNIT"
-            ],
+            "dependencies": ["truffle:TRUFFLE_API", "TRUFFLESOM", "mx:JUNIT"],
             "checkstyle": "trufflesom",
             "jacoco": "include",
             "javaCompliance": "17+",
@@ -79,42 +72,31 @@ suite = {
         },
     },
     "distributions": {
-         "TRUFFLESOM": {
-             "description": "TruffleSOM",
-             "moduleInfo": {
-                 "name": "trufflesom",
-                 "exports": [
-                     "trufflesom.* to org.graalvm.truffle",
-                 ],
-                 "requires": [
-                     "jdk.unsupported" # sun.misc.Unsafe
-                 ],
-                 "requiresConcealed": {
-                     "org.graalvm.truffle": [
-                         "com.oracle.truffle.api",
-                         "com.oracle.truffle.api.instrumentation",
-                     ],
-                 },
-             },
-             "dependencies": [
-                 "trufflesom"
-             ],
-             "distDependencies": [
-                 "truffle:TRUFFLE_API"
-             ],
-             # "javaProperties": {
-             #     "org.graalvm.language.smalltalk.home": "<path:TRUFFLESQUEAK_HOME>",
-             # },
-         },
-         "TRUFFLESOM_TEST": {
-             "description": "TruffleSOM JUnit Tests",
-             "javaCompliance": "17+",
-             "dependencies": [
-                 "tests"
-             ],
-             "exclude": ["mx:JUNIT", "mx:HAMCREST"],
-             "distDependencies": ["TRUFFLESOM"],
-             "testDistribution": True,
-         }
-     }
+        "TRUFFLESOM": {
+            "description": "TruffleSOM",
+            "moduleInfo": {
+                "name": "trufflesom",
+                "exports": [
+                    "trufflesom.* to org.graalvm.truffle",
+                ],
+                "requires": ["jdk.unsupported"],  # sun.misc.Unsafe
+                "requiresConcealed": {
+                    "org.graalvm.truffle": [
+                        "com.oracle.truffle.api",
+                        "com.oracle.truffle.api.instrumentation",
+                    ],
+                },
+            },
+            "dependencies": ["trufflesom"],
+            "distDependencies": ["truffle:TRUFFLE_API"],
+        },
+        "TRUFFLESOM_TEST": {
+            "description": "TruffleSOM JUnit Tests",
+            "javaCompliance": "17+",
+            "dependencies": ["tests"],
+            "exclude": ["mx:JUNIT", "mx:HAMCREST"],
+            "distDependencies": ["TRUFFLESOM"],
+            "testDistribution": True,
+        },
+    },
 }
