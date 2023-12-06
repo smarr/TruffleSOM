30be2bf9db5bc888d35f6782bf3101591167c022
Stefan Marr <git@stefan-marr.de>
2023-12-06 19:34:34 +0000

    Work around DSL issue, which doesn’t produce the specializations of the super classes
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/src/trufflesom/interpreter/operations/copied/SubtractionOp.java
+++ src/trufflesom/src/trufflesom/interpreter/operations/copied/SubtractionOp.java
@@ -1,5 +1,8 @@
 package trufflesom.interpreter.operations.copied;
 
+import java.math.BigInteger;
+
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.bytecode.OperationProxy.Proxyable;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.ImportStatic;
@@ -7,14 +10,78 @@ import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 import trufflesom.interpreter.nodes.dispatch.AbstractDispatchNode;
-import trufflesom.primitives.arithmetic.SubtractionPrim;
+import trufflesom.primitives.arithmetic.ArithmeticPrim;
 import trufflesom.vm.SymbolTable;
 import trufflesom.vmobjects.SObject;
+import trufflesom.vmobjects.SSymbol;
 
 
 @Proxyable
 @ImportStatic(SymbolTable.class)
-public abstract class SubtractionOp extends SubtractionPrim {
+public abstract class SubtractionOp extends ArithmeticPrim {
+
+  @Override
+  public final SSymbol getSelector() {
+    return SymbolTable.symMinus;
+  }
+
+  @Specialization(rewriteOn = ArithmeticException.class)
+  public static final long doLong(final long left, final long right) {
+    return Math.subtractExact(left, right);
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public static final Object doLongWithOverflow(final long left, final long right) {
+    return reduceToLongIfPossible(
+        BigInteger.valueOf(left).subtract(BigInteger.valueOf(right)));
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public static final Object doLong(final long left, final BigInteger right) {
+    return doBigInteger(BigInteger.valueOf(left), right);
+  }
+
+  @Specialization
+  public static final double doLong(final long left, final double right) {
+    return left - right;
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public static final Object doBigInteger(final BigInteger left, final long right) {
+    return doBigInteger(left, BigInteger.valueOf(right));
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public static final Object doDouble(final BigInteger left, final double right) {
+    return left.doubleValue() - right;
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public static final Object doBigInteger(final BigInteger left, final BigInteger right) {
+    BigInteger result = left.subtract(right);
+    return reduceToLongIfPossible(result);
+  }
+
+  @Specialization
+  public static final double doDouble(final double left, final double right) {
+    return left - right;
+  }
+
+  @Specialization
+  public static final double doDouble(final double left, final long right) {
+    return left - right;
+  }
+
+  @Specialization
+  @TruffleBoundary
+  public static final Object doDouble(final double left, final BigInteger right) {
+    return left - right.doubleValue();
+  }
 
   @Specialization
   public static final Object doCached(final VirtualFrame frame, final SObject rcvr,
