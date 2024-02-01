b66a8494a3a67e92c04d19dcfd40825bcdba0ffa
Stefan Marr <git@stefan-marr.de>
2024-02-01 17:40:53 +0000

    Use more ContextualNode.determineContextNoUnroll for BcDSL
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/src/trufflesom/interpreter/operations/copied/ReturnNonLocal.java
+++ src/trufflesom/src/trufflesom/interpreter/operations/copied/ReturnNonLocal.java
@@ -34,7 +34,7 @@ public abstract class ReturnNonLocal extends Node {
       final int onStackMarkerIndex, final int contextLevel,
       @Cached final InlinedBranchProfile blockEscaped,
       @Bind("this") final Node selfNode) {
-    MaterializedFrame ctx = ContextualNode.determineContext(frame, contextLevel);
+    MaterializedFrame ctx = ContextualNode.determineContextNoUnroll(frame, contextLevel);
     FrameOnStackMarker marker =
         (FrameOnStackMarker) ctx.getObject(onStackMarkerIndex);
 
