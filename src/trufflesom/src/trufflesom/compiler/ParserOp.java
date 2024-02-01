d00b7bc0892c2a31c4d0400cc5ac33d5e44a8d01
Stefan Marr <git@stefan-marr.de>
2024-02-01 10:58:24 +0000

    Move things around to allow for reprasing of code
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/src/trufflesom/compiler/ParserOp.java
+++ src/trufflesom/src/trufflesom/compiler/ParserOp.java
@@ -1,17 +1,18 @@
 package trufflesom.compiler;
 
-import com.oracle.truffle.api.bytecode.BytecodeConfig;
 import com.oracle.truffle.api.source.Source;
 
 import trufflesom.bdt.basic.ProgramDefinitionError;
 import trufflesom.bdt.tools.structure.StructuralProbe;
-import trufflesom.interpreter.operations.SomOperationsGen;
+import trufflesom.interpreter.operations.SomOperationsGen.Builder;
 import trufflesom.vmobjects.SClass;
 import trufflesom.vmobjects.SInvokable;
 import trufflesom.vmobjects.SSymbol;
 
 
 public class ParserOp extends ParserAst {
+
+  private Builder builder;
 
   public ParserOp(final String code, final Source source,
       final StructuralProbe<SSymbol, SClass, SInvokable, Field, Variable> probe) {
@@ -20,20 +21,11 @@ public class ParserOp extends ParserAst {
 
   @Override
   public void classdef(final ClassGenerationContext cgenc) throws ProgramDefinitionError {
-    ProgramDefinitionError[] error = new ProgramDefinitionError[1];
-
-    SomOperationsGen.create(BytecodeConfig.WITH_SOURCE, builder -> {
-      try {
-        super.classdef(cgenc);
-        cgenc.convertMethods(builder);
-      } catch (ProgramDefinitionError e) {
-        error[0] = e;
-      }
-    });
-
-    if (error[0] != null) {
-      throw error[0];
-    }
+    super.classdef(cgenc);
+    cgenc.convertMethods(builder);
   }
 
+  public void setBuilder(final Builder builder) {
+    this.builder = builder;
+  }
 }
