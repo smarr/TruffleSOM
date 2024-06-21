7ef3667581dd5f3c7d0a7ca53ae32b0894cc75bb
Stefan Marr <git@stefan-marr.de>
2024-06-21 10:23:22 +0100

    Use MatcherAssert.assertThat since Assert.assertThat is deprecated
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/trufflesom/tests/AstInliningTests.java
+++ tests/trufflesom/tests/AstInliningTests.java
@@ -1,8 +1,8 @@
 package trufflesom.tests;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
