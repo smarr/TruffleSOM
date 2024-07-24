f6c25d977d322ca8cddc066b150146f818b895fd
Stefan Marr <git@stefan-marr.de>
2024-07-24 13:16:19 +0100

    Update to latest version of Bytecode DSL
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- mx.trufflesom/suite.py
+++ mx.trufflesom/suite.py
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "truffle",
                 "subdir": True,
-                "version": "6339bcd8f1ca06eee25877838d92ca1256cbbf02",
+                "version": "7ba2ce3618f08048367bd91d5aeadbbfe739f394",
                 "urls": [{"url": "https://github.com/oracle/graal", "kind": "git"}],
             },
         ]
