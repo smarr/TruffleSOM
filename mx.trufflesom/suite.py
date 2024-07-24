c73aaf286f7c5b6293412552df55c6ed0e0cf078
Stefan Marr <git@stefan-marr.de>
2024-07-24 18:16:03 +0100

    Update to latest BcDSL with a hack for a validation issue for LOAD_LOCAL_MAT
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- mx.trufflesom/suite.py
+++ mx.trufflesom/suite.py
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "truffle",
                 "subdir": True,
-                "version": "97dd890fa435f5e33844d6353730e81df3b34b9f",
+                "version": "c0ad4336f3b25c9772bf1ed4eb27a52854b49653",
                 "urls": [{"url": "https://github.com/oracle/graal", "kind": "git"}],
             },
         ]
