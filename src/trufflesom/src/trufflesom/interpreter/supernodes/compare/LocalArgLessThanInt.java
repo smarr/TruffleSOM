39e52a598c9c25b3a87607d58ed0b1ee7386ea88
Stefan Marr <git@stefan-marr.de>
2024-11-19 17:34:20 +0000

    Implement missing constructOperation methods after rebase
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/src/trufflesom/interpreter/supernodes/compare/LocalArgLessThanInt.java
+++ src/trufflesom/src/trufflesom/interpreter/supernodes/compare/LocalArgLessThanInt.java
@@ -6,6 +6,7 @@ import com.oracle.truffle.api.nodes.UnexpectedResultException;
 import trufflesom.bdt.inlining.ScopeAdaptationVisitor;
 import trufflesom.bdt.inlining.ScopeAdaptationVisitor.ScopeElement;
 import trufflesom.compiler.Variable.Argument;
+import trufflesom.interpreter.Method.OpBuilder;
 import trufflesom.interpreter.bc.RespecializeException;
 import trufflesom.interpreter.nodes.ArgumentReadNode.LocalArgumentReadNode;
 import trufflesom.interpreter.nodes.ExpressionNode;
@@ -101,6 +102,16 @@ public final class LocalArgLessThanInt extends ExpressionNode {
       }
     } else {
       assert 0 == se.contextLevel;
+    }
+  }
+
+  @Override
+  public void constructOperation(final OpBuilder opBuilder, boolean resultUsed) {
+    if (resultUsed) {
+      opBuilder.dsl.beginLessThanPrim();
+      opBuilder.dsl.emitLoadArgument(argIdx);
+      opBuilder.dsl.emitLoadConstant(intValue);
+      opBuilder.dsl.endLessThanPrim();
     }
   }
 }
