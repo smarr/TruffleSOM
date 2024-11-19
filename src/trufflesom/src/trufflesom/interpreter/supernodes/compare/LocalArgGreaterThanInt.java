3eebe6e51ecea7944ce7c7bb79e02636ecc40f33
Stefan Marr <git@stefan-marr.de>
2024-11-19 17:35:02 +0000

    Fallback to super node instead of to generic message send on inlining
    This is when an arg is turned into an inlined local.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/src/trufflesom/interpreter/supernodes/compare/LocalArgGreaterThanInt.java
+++ src/trufflesom/src/trufflesom/interpreter/supernodes/compare/LocalArgGreaterThanInt.java
@@ -96,10 +96,7 @@ public final class LocalArgGreaterThanInt extends ExpressionNode {
       if (se.var instanceof Argument arg) {
         replace(new LocalArgGreaterThanInt(arg, intValue).initialize(sourceCoord));
       } else {
-        replace(MessageSendNode.createGeneric(SymbolTable.symbolFor(">"),
-            new ExpressionNode[] {se.var.getReadNode(se.contextLevel, sourceCoord),
-                new IntegerLiteralNode(intValue)},
-            sourceCoord));
+        replace(GreaterThanIntNodeGen.create(intValue, se.var.getReadNode(se.contextLevel, sourceCoord)));
       }
     } else {
       assert 0 == se.contextLevel;
