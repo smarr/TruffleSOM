9cf38ff3c42573aeac43092ef6615e5e73ca9a81
Stefan Marr <git@stefan-marr.de>
2025-02-23 18:19:02 +0000

    Change *aryMsgExprNode fallback to a cached dispatch instead of a rewrite
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/src/trufflesom/interpreter/nodes/nary/BinaryMsgExprNode.java
+++ src/trufflesom/src/trufflesom/interpreter/nodes/nary/BinaryMsgExprNode.java
@@ -2,10 +2,12 @@ package trufflesom.interpreter.nodes.nary;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Bind;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 import com.oracle.truffle.api.nodes.Node;
+import trufflesom.interpreter.nodes.dispatch.AbstractDispatchNode;
 import trufflesom.vmobjects.SSymbol;
 
 
@@ -13,12 +15,16 @@ public abstract class BinaryMsgExprNode extends BinaryExpressionNode {
 
   public abstract SSymbol getSelector();
 
+  public static AbstractDispatchNode createDispatch(Node node) {
+    BinaryMsgExprNode n = (BinaryMsgExprNode) node;
+    return AbstractDispatchNode.create(n.getSelector());
+  }
+
   @Fallback
   public static final Object makeGenericSend(final VirtualFrame frame,
-      final Object receiver, final Object argument, @Bind Node s) {
-    CompilerDirectives.transferToInterpreterAndInvalidate();
-    BinaryMsgExprNode self = (BinaryMsgExprNode) s;
-    return self.makeGenericSend(self.getSelector()).doPreEvaluated(frame,
-        new Object[] {receiver, argument});
+      final Object receiver, final Object argument,
+      @Bind Node self,
+      @Cached("createDispatch(self)") final AbstractDispatchNode dispatch) {
+    return dispatch.executeDispatch(frame, new Object[] {receiver, argument});
   }
 }
