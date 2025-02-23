b16cd51de3a451057d7aa6588163ac7f39c410e7
Stefan Marr <git@stefan-marr.de>
2025-02-23 18:19:02 +0000

    Change *aryMsgExprNode fallback to a cached dispatch instead of a rewrite
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/trufflesom/src/trufflesom/interpreter/nodes/nary/QuaternaryMsgExprNode.java
+++ src/trufflesom/src/trufflesom/interpreter/nodes/nary/QuaternaryMsgExprNode.java
@@ -2,23 +2,27 @@ package trufflesom.interpreter.nodes.nary;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Bind;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 import com.oracle.truffle.api.nodes.Node;
+import trufflesom.interpreter.nodes.dispatch.AbstractDispatchNode;
 import trufflesom.vmobjects.SSymbol;
 
 
 public abstract class QuaternaryMsgExprNode extends QuaternaryExpressionNode {
   public abstract SSymbol getSelector();
 
+  public static AbstractDispatchNode createDispatch(Node node) {
+    QuaternaryMsgExprNode n = (QuaternaryMsgExprNode) node;
+    return AbstractDispatchNode.create(n.getSelector());
+  }
+
   @Fallback
   public static final Object makeGenericSend(final VirtualFrame frame,
       final Object receiver, final Object arg1, final Object arg2, final Object arg3,
-      @Bind Node s) {
-    CompilerDirectives.transferToInterpreterAndInvalidate();
-    QuaternaryMsgExprNode self = (QuaternaryMsgExprNode) s;
-    return self.makeGenericSend(self.getSelector()).doPreEvaluated(frame,
-        new Object[] {receiver, arg1, arg2, arg3});
+      @Bind Node self, @Cached("createDispatch(self)") final AbstractDispatchNode dispatch) {
+    return dispatch.executeDispatch(frame, new Object[] {receiver, arg1, arg2, arg3});
   }
 }
