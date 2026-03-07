3287a0369f84ab07934e3aa0f847ee449fcc359c
Stefan Marr <git@stefan-marr.de>
2026-03-07 02:23:14 +0100

    Update Truffle to version of 2026-03-07

--- mx.trufflesom/suite.py
+++ mx.trufflesom/suite.py
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "truffle",
                 "subdir": True,
-                "version": "cbb1b8c08127400cb41a48d70f1baf51bf2e21c9",
+                "version": "11c4ef13ce131e3615d0a0ba93da70daaccbabce",
                 "urls": [{"url": "https://github.com/oracle/graal", "kind": "git"}],
             },
         ]
