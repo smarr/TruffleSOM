ac030a34aebc1ce6111af609a715a6f25bd2462b
Stefan Marr <git@stefan-marr.de>
2026-09-26 02:23:19 +0200

    Update Truffle, JDK, GraalVM EE to version of 2026-09-26
    - Truffle: 1158430 -> a31054b
    - JDK: temurin-25.0.2+10.0.LTS -> temurin-25.0.4+101.0.LTS
    - GraalVM EE: oracle-graalvm-ea-jdk-25i2-25.0.3-ea.04 -> oracle-graalvm-ea-jdk-25i5-25.0.4.1.1-ea.01

--- mx.trufflesom/suite.py
+++ mx.trufflesom/suite.py
@@ -16,7 +16,7 @@ suite = {
             {
                 "name": "truffle",
                 "subdir": True,
-                "version": "11584300a523d723717fb8c0958841f75282f2b8",
+                "version": "a31054b53f56d6276e7d447627a4b608d48f7aee",
                 "urls": [{"url": "https://github.com/oracle/graal", "kind": "git"}],
             },
         ]
